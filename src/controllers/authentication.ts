cec84809ecf87568145a92a28c1737da129a090d
Donato Amasa <d.amasa@xt.martrust.com>
2024-01-31 11:38:49 +0800

    removed await

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -9,7 +9,7 @@ export const register = async(req: express.Request, res: express.Response) => {
             return res.sendStatus(400);
         }
 
-        const user = await saveUser(username, email, password);
+        const user = saveUser(username, email, password);
 
         return res.status(200).json(user).end();
     } catch (error) {
